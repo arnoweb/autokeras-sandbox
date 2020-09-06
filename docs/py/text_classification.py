9222fcf97d8ea1d2d965cab4a5fa5a5de46ddb69
arnoweb <arnbret@gmail.com>
2020-09-06 21:38:02 +0200

    Copy from autokeras source files

---
+++ docs/py/text_classification.py
@@ -0,0 +1,179 @@
+"""shell
+pip install autokeras
+pip install git+https://github.com/keras-team/keras-tuner.git@1.0.2rc1
+"""
+
+"""
+## A Simple Example
+The first step is to prepare your data. Here we use the [IMDB
+dataset](https://keras.io/datasets/#imdb-movie-reviews-sentiment-classification) as
+an example.
+"""
+
+import numpy as np
+from tensorflow.keras.datasets import imdb
+
+# Load the integer sequence the IMDB dataset with Keras.
+index_offset = 3  # word index offset
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=1000,
+                                                      index_from=index_offset)
+y_train = y_train.reshape(-1, 1)
+y_test = y_test.reshape(-1, 1)
+# Prepare the dictionary of index to word.
+word_to_id = imdb.get_word_index()
+word_to_id = {k: (v + index_offset) for k, v in word_to_id.items()}
+word_to_id["<PAD>"] = 0
+word_to_id["<START>"] = 1
+word_to_id["<UNK>"] = 2
+id_to_word = {value: key for key, value in word_to_id.items()}
+# Convert the word indices to words.
+x_train = list(map(lambda sentence: ' '.join(
+    id_to_word[i] for i in sentence), x_train))
+x_test = list(map(lambda sentence: ' '.join(
+    id_to_word[i] for i in sentence), x_test))
+x_train = np.array(x_train, dtype=np.str)
+x_test = np.array(x_test, dtype=np.str)
+print(x_train.shape)  # (25000,)
+print(y_train.shape)  # (25000, 1)
+print(x_train[0][:50])  # <START> this film was just brilliant casting <UNK>
+
+"""
+The second step is to run the [TextClassifier](/text_classifier).
+"""
+
+import autokeras as ak
+
+# Initialize the text classifier.
+clf = ak.TextClassifier(
+    overwrite=True,
+    max_trials=1) # It tries 10 different models.
+# Feed the text classifier with training data.
+clf.fit(x_train, y_train, epochs=2)
+# Predict with the best model.
+predicted_y = clf.predict(x_test)
+# Evaluate the best model with testing data.
+print(clf.evaluate(x_test, y_test))
+
+
+"""
+## Validation Data
+By default, AutoKeras use the last 20% of training data as validation data.
+As shown in the example below, you can use `validation_split` to specify the percentage.
+"""
+
+clf.fit(x_train,
+        y_train,
+        # Split the training data and use the last 15% as validation data.
+        validation_split=0.15)
+
+"""
+You can also use your own validation set
+instead of splitting it from the training data with `validation_data`.
+"""
+
+split = 5000
+x_val = x_train[split:]
+y_val = y_train[split:]
+x_train = x_train[:split]
+y_train = y_train[:split]
+clf.fit(x_train,
+        y_train,
+        epochs=2,
+        # Use your own validation set.
+        validation_data=(x_val, y_val))
+
+"""
+## Customized Search Space
+For advanced users, you may customize your search space by using
+[AutoModel](/auto_model/#automodel-class) instead of
+[TextClassifier](/text_classifier). You can configure the
+[TextBlock](/block/#textblock-class) for some high-level configurations, e.g., `vectorizer`
+for the type of text vectorization method to use.  You can use 'sequence', which uses
+[TextToInteSequence](/block/#texttointsequence-class) to convert the words to
+integers and use [Embedding](/block/#embedding-class) for embedding the
+integer sequences, or you can use 'ngram', which uses
+[TextToNgramVector](/block/#texttongramvector-class) to vectorize the
+sentences.  You can also do not specify these arguments, which would leave the
+different choices to be tuned automatically.  See the following example for detail.
+"""
+
+import autokeras as ak
+
+input_node = ak.TextInput()
+output_node = ak.TextBlock(vectorizer='ngram')(input_node)
+output_node = ak.ClassificationHead()(output_node)
+clf = ak.AutoModel(
+    inputs=input_node,
+    outputs=output_node,
+    overwrite=True,
+    max_trials=1)
+clf.fit(x_train, y_train, epochs=2)
+
+"""
+The usage of [AutoModel](/auto_model/#automodel-class) is similar to the
+[functional API](https://www.tensorflow.org/guide/keras/functional) of Keras.
+Basically, you are building a graph, whose edges are blocks and the nodes are intermediate outputs of blocks.
+To add an edge from `input_node` to `output_node` with
+`output_node = ak.[some_block]([block_args])(input_node)`.
+
+You can even also use more fine grained blocks to customize the search space even
+further. See the following example.
+"""
+
+import autokeras as ak
+
+input_node = ak.TextInput()
+output_node = ak.TextToIntSequence()(input_node)
+output_node = ak.Embedding()(output_node)
+# Use separable Conv layers in Keras.
+output_node = ak.ConvBlock(separable=True)(output_node)
+output_node = ak.ClassificationHead()(output_node)
+clf = ak.AutoModel(
+    inputs=input_node,
+    outputs=output_node,
+    overwrite=True,
+    max_trials=1)
+clf.fit(x_train, y_train, epochs=2)
+
+"""
+## Data Format
+The AutoKeras TextClassifier is quite flexible for the data format.
+
+For the text, the input data should be one-dimensional 
+For the classification labels, AutoKeras accepts both plain labels, i.e. strings or
+integers, and one-hot encoded encoded labels, i.e. vectors of 0s and 1s.
+
+We also support using [tf.data.Dataset](
+https://www.tensorflow.org/api_docs/python/tf/data/Dataset?version=stable) format for
+the training data.
+The labels have to be one-hot encoded for multi-class
+classification to be wrapped into tensorflow Dataset.
+Since the IMDB dataset is binary classification, it should not be one-hot encoded.
+"""
+
+import tensorflow as tf
+train_set = tf.data.Dataset.from_tensor_slices(((x_train, ), (y_train, ))).batch(32)
+test_set = tf.data.Dataset.from_tensor_slices(((x_test, ), (y_test, ))).batch(32)
+
+clf = ak.TextClassifier(
+    overwrite=True,
+    max_trials=3)
+# Feed the tensorflow Dataset to the classifier.
+clf.fit(train_set, epochs=2)
+# Predict with the best model.
+predicted_y = clf.predict(test_set)
+# Evaluate the best model with testing data.
+print(clf.evaluate(test_set))
+
+"""
+## Reference
+[TextClassifier](/text_classifier),
+[AutoModel](/auto_model/#automodel-class),
+[TextBlock](/block/#textblock-class),
+[TextToInteSequence](/block/#texttointsequence-class),
+[Embedding](/block/#embedding-class),
+[TextToNgramVector](/block/#texttongramvector-class),
+[ConvBlock](/block/#convblock-class),
+[TextInput](/node/#textinput-class),
+[ClassificationHead](/block/#classificationhead-class).
+"""
---
+++ tests/utils.py
@@ -0,0 +1,120 @@
+# Copyright 2020 The AutoKeras Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import inspect
+import os
+
+import numpy as np
+import tensorflow as tf
+
+import autokeras as ak
+
+SEED = 5
+COLUMN_NAMES = [
+    "sex",
+    "age",
+    "n_siblings_spouses",
+    "parch",
+    "fare",
+    "class",
+    "deck",
+    "embark_town",
+    "alone",
+]
+COLUMN_TYPES = {
+    "sex": "categorical",
+    "age": "numerical",
+    "n_siblings_spouses": "categorical",
+    "parch": "categorical",
+    "fare": "numerical",
+    "class": "categorical",
+    "deck": "categorical",
+    "embark_town": "categorical",
+    "alone": "categorical",
+}
+TRAIN_DATA_URL = "https://storage.googleapis.com/tf-datasets/titanic/train.csv"
+TEST_DATA_URL = "https://storage.googleapis.com/tf-datasets/titanic/eval.csv"
+
+TRAIN_CSV_PATH = tf.keras.utils.get_file(
+    fname=os.path.basename(TRAIN_DATA_URL), origin=TRAIN_DATA_URL
+)
+TEST_CSV_PATH = tf.keras.utils.get_file(
+    fname=os.path.basename(TEST_DATA_URL), origin=TEST_DATA_URL
+)
+
+
+def generate_data(num_instances=100, shape=(32, 32, 3), dtype="np"):
+    np.random.seed(SEED)
+    data = np.random.rand(*((num_instances,) + shape))
+    if data.dtype == np.float64:
+        data = data.astype(np.float32)
+    if dtype == "np":
+        return data
+    if dtype == "dataset":
+        return tf.data.Dataset.from_tensor_slices(data)
+
+
+def generate_one_hot_labels(num_instances=100, num_classes=10, dtype="np"):
+    np.random.seed(SEED)
+    labels = np.random.randint(num_classes, size=num_instances)
+    data = tf.keras.utils.to_categorical(labels)
+    if dtype == "np":
+        return data
+    if dtype == "dataset":
+        return tf.data.Dataset.from_tensor_slices(data).batch(32)
+
+
+def imdb_raw(num_instances=100):
+    index_offset = 3  # word index offset
+
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.imdb.load_data(
+        num_words=1000, index_from=index_offset
+    )
+    if num_instances is not None:
+        x_train = x_train[:num_instances]
+        y_train = y_train[:num_instances].reshape(-1, 1)
+        x_test = x_test[:num_instances]
+        y_test = y_test[:num_instances].reshape(-1, 1)
+
+    word_to_id = tf.keras.datasets.imdb.get_word_index()
+    word_to_id = {k: (v + index_offset) for k, v in word_to_id.items()}
+    word_to_id["<PAD>"] = 0
+    word_to_id["<START>"] = 1
+    word_to_id["<UNK>"] = 2
+
+    id_to_word = {value: key for key, value in word_to_id.items()}
+    x_train = list(
+        map(lambda sentence: " ".join(id_to_word[i] for i in sentence), x_train)
+    )
+    x_test = list(
+        map(lambda sentence: " ".join(id_to_word[i] for i in sentence), x_test)
+    )
+    x_train = np.array(x_train, dtype=np.str)
+    x_test = np.array(x_test, dtype=np.str)
+    return (x_train, y_train), (x_test, y_test)
+
+
+def build_graph():
+    tf.keras.backend.clear_session()
+    image_input = ak.ImageInput(shape=(32, 32, 3))
+    merged_outputs = ak.ImageBlock()(image_input)
+    head = ak.ClassificationHead(num_classes=10)
+    head.output_shape = (10,)
+    classification_outputs = head(merged_outputs)
+    return ak.graph.Graph(inputs=image_input, outputs=classification_outputs)
+
+
+def get_func_args(func):
+    params = inspect.signature(func).parameters.keys()
+    return set(params) - set(["self", "args", "kwargs"])
